01f2db239e6f881ced91ceda4e587b2d7a016a67
Sujoy Paul <freelancersujoy@gmail.com>
2023-01-10 12:23:46 +0600

    basic mongoDB setup complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,6 +9,19 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORd}@cluster0.6ke0m0t.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        
+    }
+    finally{}
+}
+run().catch(err => {
+    console.error(err);
+})
 
 app.get('/', (req, res) => {
   res.send(`
