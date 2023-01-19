2d6570588c4cb0dec1e0a67a3377e399454bc337
Sujoy Paul <freelancersujoy@gmail.com>
2023-01-20 01:49:07 +0600

    delete api created

--- index.js
+++ index.js
@@ -272,6 +272,14 @@ async function run(){
             res.send(result) 
         })
 
+        //delete an user from database
+        app.delete('/profile/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
 
     finally{
